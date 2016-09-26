e2b7a6264063c6e0b855ceb6d8d1bd65f151825d
uniquexiaobai <unique_xiaobai@163.com>
2016-09-26 12:12:03 +0800

    [#27] BroadcastReceiver

---
+++ BroadcastReceiver/src/main/java/com/uniquexiaobai/androiddemos/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,22 @@
+package com.uniquexiaobai.androiddemos.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by uniqu on 2016/9/26.
+ */
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() == "android.net.conn.CONNECTIVITY_CHANGE") {
+            Toast.makeText(context, "网络状况变化了", Toast.LENGTH_SHORT).show();
+        }
+        if (intent.getAction() == "com.xiaobai.123") {
+            Toast.makeText(context, "接收到的广播内容为：" + intent.getStringExtra("a"), Toast.LENGTH_SHORT).show();
+        }
+    }
+}

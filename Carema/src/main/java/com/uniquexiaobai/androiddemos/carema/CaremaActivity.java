7dc70cdec1dd1b4cd0d97e6f99cee02743c79bd0
uniquexiaobai <unique_xiaobai@163.com>
2016-09-26 19:22:21 +0800

    [#29] Carema

---
+++ Carema/src/main/java/com/uniquexiaobai/androiddemos/carema/CaremaActivity.java
@@ -0,0 +1,41 @@
+package com.uniquexiaobai.androiddemos.carema;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CaremaActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carema);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, Activity.DEFAULT_KEYS_DIALER);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
